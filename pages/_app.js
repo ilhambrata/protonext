041f080e2289671193b5adf61fe16d1ce8345699
ilhammeidi <ilham.brata@inmagine.com>
2019-06-17 16:41:30 +0700

    first commiy'

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider, StylesProvider, jssPreset } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../components/theme';
+
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+    return (
+      <Container>
+        <Head>
+          <title>My pagexxxx</title>
+        </Head>
+        <StylesProvider jss={jss}>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </StylesProvider>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
