85458481b976f160975ff0311fd4d0595952bdaf
ilhammeidi <ilham.brata@inmagine.com>
2019-07-01 10:08:29 +0700

    remove unused file

--- i18n.js
+++ i18n.js
@@ -1,9 +1,8 @@
 const NextI18Next = require('next-i18next/dist/commonjs');
-const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 
 module.exports = new NextI18Next({
   defaultLanguage: 'en',
   otherLanguages: ['de'],
   lng: 'en',
-  localeSubpaths,
+  localeSubpaths: 'foreign',
 });
